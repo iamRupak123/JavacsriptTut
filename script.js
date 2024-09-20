86d9283f62ef94408be047208a950b62f498a9dc
Rupak Ghosh <rupakghosh10101@gmail.com>
2024-09-20 23:43:10 +0530

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("hello world")
+var num = prompt("Enter your marks:");
+
+if(num<=100 && num>=90){
+    console.log("A");
+}
+else if(num<90 && num>=80){
+    console.log("B");
+}
+else if(num<80 && num>=70){
+    console.log("C");
+}
+else if(num<70 && num>=60){
+    console.log("D")
+}
+else{
+    console.log("F");
+}
